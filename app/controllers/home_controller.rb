9b4a208691566ebf07873cae6f46476d709a2fb6
Chuong1928 <6789tyh@gmail.com>
2021-09-10 17:27:22 +0700

    [No 3] : [SEARCH | FILTER]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   layout "app"
-   before_action :authenticate_user!
+  before_action :authenticate_user!
   def index
+    @types = FilmType.all
+    @search = Film.search(params[:q])
+    @films = @search.result
+    respond_to do |format|
+      format.html # index.html.erb
+      
+  end
   end
 end
---
+++ test/models/film_type_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FilmTypeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
