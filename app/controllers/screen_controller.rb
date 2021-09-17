411beb1656dc7c74a72be36330fe79c1574d9588
Chuong1928 <6789tyh@gmail.com>
2021-09-17 16:04:51 +0700

    [No 6] : [SEATORDER]

--- app/controllers/screen_controller.rb
+++ app/controllers/screen_controller.rb
@@ -1,3 +1,33 @@
 class ScreenController < ApplicationController
+    before_action :authenticate_user!
+    layout "app"
+    def index
+        @films = Film.all
+        data_filter = params[:filter]
+         if !data_filter.nil?
+             @screenings = Screening.where(day_show_film: params[:filter][:date])
+             p "di vao day"
+         else
+            @screenings = Screening.where(day_show_film: Time.now.strftime("%d/%m/%Y"))
+         end
+        
+        respond_to do |format|
+            format.html # index.html.erb
+            format.js
+        end
+    end
+    def show
+        @this_film = Film.find(params[:film_id])
+        @screening = Screening.find(params[:id])
+        respond_to do |format|
+            format.html # index.html.erb
+            format.js
+        end
+    end
+    def payment
+        @this_film = Film.find(params[:film_id])
+        @screening = Screening.find(params[:id])
+        @foods = Food.all
+    end
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_044722) do
+ActiveRecord::Schema.define(version: 2021_09_17_031900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,22 @@ ActiveRecord::Schema.define(version: 2021_09_13_044722) do
     t.text "plot"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "foodorders", force: :cascade do |t|
+    t.integer "food_id"
+    t.integer "order_id"
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "url_img"
   end
 
   create_table "orders", force: :cascade do |t|
